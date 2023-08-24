a8d9adc6e15a0cf3c6000db4aa4e116b40e2378b
Vusi-Gae <vusigae@gmail.com>
2023-08-24 12:19:06 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- set_op_toks_error.c
+++ set_op_toks_error.c
@@ -6,31 +6,31 @@
  */
 void set_op_tok_error(int error_code)
 {
-	int toks_leng = 0, i = 0;
-	char *exit_string = NULL;
+	int toks_len = 0, t = 0;
+	char *exit_str = NULL;
 	char **new_toks = NULL;
 
-	toks_leng = token_arr_len();
-	new_toks = malloc(sizeof(char *) * (toks_leng + 2));
+	toks_len = token_arr_len();
+	new_toks = malloc(sizeof(char *) * (toks_len + 2));
 	if (!op_toks)
 	{
 		malloc_error();
 		return;
 	}
-	while (i < toks_leng)
+	while (t < toks_len)
 	{
-		new_toks[i] = op_toks[i];
-		i++;
+		new_toks[t] = op_toks[t];
+		t++;
 	}
-	exit_string = get_int(error_code);
-	if (!exit_string)
+	exit_str = get_int(error_code);
+	if (!exit_str)
 	{
 		free(new_toks);
 		malloc_error();
 		return;
 	}
-	new_toks[i++] = exit_string;
-	new_toks[i] = NULL;
+	new_toks[t++] = exit_str;
+	new_toks[t] = NULL;
 	free(op_toks);
 	op_toks = new_toks;
 }
